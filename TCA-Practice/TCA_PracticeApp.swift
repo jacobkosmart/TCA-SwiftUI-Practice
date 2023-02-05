d6f858852638d35d711d6a147ec828adeb0a78c6
jacobkosmart <jacobkosmart@gmail.com>
2023-02-05 14:26:08 +0900

    Initial Commit

---
+++ TCA-Practice/TCA_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_PracticeApp.swift
+//  TCA-Practice
+//
+//  Created by Jacob Ko on 2023/02/05.
+//
+
+import SwiftUI
+
+@main
+struct TCA_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
